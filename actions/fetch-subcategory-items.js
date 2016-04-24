cde06d328bc0584b4caf277eb07819ac57a36533
Meysam <meysam.ghaderyan@macys.com>
2016-04-24 15:12:26 -0700

    sneakily send generic messages!

--- actions/fetch-subcategory-items.js
+++ actions/fetch-subcategory-items.js
@@ -2,6 +2,38 @@
 
 const Promise = require('bluebird');
 
+const subcategoryItemsPayload = {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "generic",
+            "elements": [{
+                "title": "First card",
+                "subtitle": "Element #1 of an hscroll",
+                "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                "buttons": [{
+                    "type": "web_url",
+                    "url": "https://www.messenger.com",
+                    "title": "web url"
+                }, {
+                    "type": "postback",
+                    "title": "Postback",
+                    "payload": "Payload for first element in a generic bubble",
+                }],
+            }, {
+                "title": "Second card",
+                "subtitle": "Element #2 of an hscroll",
+                "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                "buttons": [{
+                    "type": "postback",
+                    "title": "Postback",
+                    "payload": "Payload for second element in a generic bubble",
+                }],
+            }]
+        }
+    }
+};
+
 function getSubCategoryItems(selectedSubcategory) {
   return Promise.resolve('blah');
 }
@@ -10,6 +42,7 @@ module.exports = (sessionId, context, cb) => {
   getSubCategoryItems(context.selectedSubcategory)
   .then((subcategoryItems) => {
     context.subcategoryItems = subcategoryItems;
+    context.subcategoryItemsPayload = subcategoryItemsPayload;
     cb(context);
   });
 };
--- actions/index.js
+++ actions/index.js
@@ -3,37 +3,15 @@ const firstEntityValue = require('./firstEntityValue');
 const fetchSubCategories = require('./fetch-sub-categories');
 const fetchSubcategoryItems = require('./fetch-subcategory-items');
 
-const messageData = {
-    "attachment": {
-        "type": "template",
-        "payload": {
-            "template_type": "generic",
-            "elements": [{
-                "title": "First card",
-                "subtitle": "Element #1 of an hscroll",
-                "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                "buttons": [{
-                    "type": "web_url",
-                    "url": "https://www.messenger.com",
-                    "title": "web url"
-                }, {
-                    "type": "postback",
-                    "title": "Postback",
-                    "payload": "Payload for first element in a generic bubble",
-                }],
-            }, {
-                "title": "Second card",
-                "subtitle": "Element #2 of an hscroll",
-                "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                "buttons": [{
-                    "type": "postback",
-                    "title": "Postback",
-                    "payload": "Payload for second element in a generic bubble",
-                }],
-            }]
-        }
-    }
-};
+function convertMessage(message, context) {
+  switch (message) {
+    case 'GENERIC_subcategoryItemsPayload':
+      return context.subcategoryItemsPayload;
+      break;
+    default:
+      return message;
+  }
+}
 
 module.exports = (fbMessage) => {
   const actions = {
@@ -45,7 +23,8 @@ module.exports = (fbMessage) => {
         // Yay, we found our recipient!
         // Let's forward our bot response to her.
         console.log('right before calling fbMessage in say method', 'context is:', context, 'message is:', message);
-        fbMessage(recipientId, message, (err, data) => {
+        var messageToSend = convertMessage(message, context);
+        fbMessage(recipientId, messageToSend, (err, data) => {
           if (err) {
             console.log(
               'Oops! An error occurred while forwarding the response to',
